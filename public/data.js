83639d7a2e5f68b079978871a25002ca625288dd
ragh01 <65010097+ragh01@users.noreply.github.com>
2020-11-30 16:36:59 +0530

    Add files via upload

---
+++ public/data.js
@@ -0,0 +1,110 @@
+data = {
+  "customers": [
+    {
+      "id": 0,
+      "first_name": "Bernice",
+      "last_name": "West",
+      "email": "comesug@leced.org",
+      "address": null,
+      "zipcode": null,
+      "city": null,
+      "avatar": "https://robohash.org/20d4467a68b71c332ed729fdce9580ff.png",
+      "birthday": null,
+      "first_seen": "2016-07-14T09:43:11.108Z",
+      "last_seen": "2016-09-23T17:56:42.847Z",
+      "has_ordered": false,
+      "latest_purchase": null,
+      "has_newsletter": true,
+      "groups": [
+        "regular"
+      ],
+      "nb_commands": 0,
+      "total_spent": 0
+    }
+  ],
+  "categories": [
+    {
+      "id": 0,
+      "name": "abstract"
+    }
+  ],
+  "products": [
+    {
+      "id": 0,
+      "category_id": 0,
+      "reference": "ab-jjkre-X",
+      "width": 30.16,
+      "height": 23.32,
+      "price": 39.75,
+      "thumbnail": "https://lorempixel.com/302/233/abstract/1",
+      "image": "https://lorempixel.com/1508/1166/abstract/1",
+      "description": "Aboruc ihag zov pawdu lid ur kalha kas ka faordur ha ot ewo letme. Rigouj omfo izasezwek rit podpo awawoz luvi afdalewa siveji mudogotun cocfeh bohsalof roje zumzac negopow liw anpo. Duca cu ehla dopwaj gube relof hevaf ve raplilsum ungismo hoh mud zujefe. Cuut toohi cahabje heehe ajeta di talirli pemre bozzujah kuv nodid uf ruj wibeb asowe guzunuz. Cuduhe sokro gohi uhfurzul ud zaca tip tisedoj mupna fabutbi hu dur kiz ki fewjuv bide nupca. Teeb zireham juw icu suf hip nibcuw redwif po cocul oporak ji si egobem ba gija.",
+      "stock": 33
+    }
+  ],
+  "commands": [
+    {
+      "id": 0,
+      "reference": "bzsmni",
+      "date": "2017-01-08T03:02:44.587Z",
+      "customer_id": 0,
+      "basket": [
+        {
+          "product_id": 0,
+          "quantity": 4
+        },
+        {
+          "product_id": 66,
+          "quantity": 1
+        }
+      ],
+      "total_ex_taxes": 135.79,
+      "delivery_fees": 7.47,
+      "tax_rate": 0.2,
+      "taxes": 28.65,
+      "total": 171.91,
+      "status": "cancelled",
+      "returned": false
+    }
+  ],
+  "reviews": [
+    {
+      "id": 0,
+      "date": "2017-01-10T21:01:55.209Z",
+      "status": "rejected",
+      "command_id": 0,
+      "product_id": 0,
+      "customer_id": 0,
+      "rating": 5,
+      "comment": "THIS IS A TEST"
+    }
+  ],
+  "settings": [
+    {
+      "id": 1,
+      "configuration": {
+        "url": "http://posters-galore.com/",
+        "mail": {
+          "sender": "julio@posters-galore.com",
+          "transport": {
+            "service": "fakemail",
+            "auth": {
+              "user": "fake@mail.com",
+              "pass": "f00b@r"
+            }
+          }
+        },
+        "file_type_whiltelist": [
+          "txt",
+          "doc",
+          "docx",
+          "xls",
+          "xlsx",
+          "pdf",
+          "png",
+          "jpg"
+        ]
+      }
+    }
+  ]
+}
